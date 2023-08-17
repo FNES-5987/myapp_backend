2e859a4d396d4c6f9aa9a1da6c747941b4adfdd1
JBJ <maystar5987@gmail.com>
2023-08-17 11:24:19 +0900

    버그해결중

---
+++ src/main/java/com/jjb/myapp/post/Post.java
@@ -0,0 +1,25 @@
+package com.jjb.myapp.post;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long no;
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private String content;
+    private String creatorName;
+    private Long createdTime;
+}
---
+++ src/main/java/com/jjb/myapp/post/PostController.java
@@ -0,0 +1,65 @@
+package com.jjb.myapp.post;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+
+
+@RequestMapping(value = "/posts")
+public class PostController {
+
+    @Autowired
+    PostRepository repo;
+
+
+    @GetMapping
+    public List<Post> getPostList() {
+
+        List<Post> list = repo.findAll(Sort.by("no").ascending());
+
+        return list;
+    }
+
+    @PostMapping
+    public ResponseEntity<Map<String, Object>> addPost(@RequestBody Post post) {
+
+        if ((post.getTitle() == null) || (post.getContent() == null) || post.getTitle().isEmpty() || post.getContent().isEmpty()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+        post.setCreatedTime(new Date().getTime());
+
+        Post savedPost = repo.save(post);
+
+
+        if (savedPost != null) {
+            Map<String, Object> res = new HashMap<>();
+            res.put("data", savedPost);
+            res.put("message", "created");
+
+            return ResponseEntity.status(HttpStatus.CREATED).body(res);
+        }
+
+
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping(value = "/{no}")
+    public ResponseEntity removePost(@PathVariable long no) {
+        System.out.println(no);
+
+        Optional<Post> post = repo.findPostByNo(no);
+
+
+        if (!post.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        repo.deleteById(no);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+}
